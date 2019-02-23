6123dd20bde14253bad83647a4f8ac6d808bd2de
Ciaran Barron <ciaran.barron.1@ucdconnect.ie>
2019-02-23 13:15:10 +0000

    changed wait time between api calls

--- APIScraper.py
+++ APIScraper.py
@@ -25,7 +25,7 @@ ENG ="mysql+mysqldb://{0}:{1}@{2}:{3}/{4}".format(LOG,PWD,URL,PORT,DB)
 
 engine = sqla.create_engine(ENG,echo=False)
 
-def scrape_dynamic_data():
+def scrape_dynamic_data(count, err):
     
     response = req.get('https://api.jcdecaux.com/vls/v1/stations?contract={0}&apiKey={1}'.format(Contract,Key))
     Data = pd.DataFrame(response.json())
@@ -59,23 +59,29 @@ def scrape_dynamic_data():
 
         try: 
             engine.execute(SQL)
+            count += 1
 
         except:
+            err += 1
             pass
+    
+    return count,err
 
 def continuous_scrape():
     
     while True:
         
+        count, err = 0,0
         S = time.time()
         #scrape data and write to RDS DB
-        scrape_dynamic_data()
+        count, err = scrape_dynamic_data(count,err)
 
         E = time.time()
-
-        dtime=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-        print('Data written to DublinBikesDB.dynamic at {}'.format(dtime))
+        runtime = E - S
         
-        time.sleep(300-(S-E))
+        dtime=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+        print('{} lines ({} errors) written to DublinBikesDB.dynamic at {}. Run time: {:.4} Sec'.format(count, err, dtime, runtime))
+        
+        time.sleep(300-(E-S))
         
 continuous_scrape()
