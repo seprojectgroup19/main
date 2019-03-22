04645e0424a0c96dfb41ddf1ae959e41e215765c
Daniel Raftery <daniel.raftery@ucdconnect.ie>
2019-03-22 13:29:08 +0000

    Forgot darksky API key

--- Authenticator.py
+++ Authenticator.py
@@ -4,7 +4,7 @@ import sqlalchemy as sqla
 def read_auth():
     """
     Function to read authentication file to save time for each script
-    :return: Key for JCDecaux, JCDecaux contract, engine for SQL database
+    :return: API key for JCDecaux, JCDecaux contract, API key for darksky, SQL database name, engine for SQL database
     """
     with open("../authentication.txt") as f:
         auth = f.read().split("\n")
@@ -12,6 +12,7 @@ def read_auth():
     # Authentication data from file
     bikes_key = auth[0]
     contract = auth[1]
+    darksky_key = auth[2]
     url = auth[3]
     log = auth[4]
     pwd = auth[5]
@@ -21,4 +22,4 @@ def read_auth():
     # Connect to SQL database
     eng = "mysql+mysqldb://{0}:{1}@{2}:{3}/{4}".format(log, pwd, url, port, db)
     engine = sqla.create_engine(eng, echo=False)
-    return bikes_key, contract, engine, db
+    return [bikes_key, contract, darksky_key, db, engine]
