c7f7d3363663c3a379b45ff5e43e580b16100b78
Cormac English <cormacenglish@gmail.com>
2019-03-27 23:34:15 +0000

    CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,6 @@
 from flask import Flask
-from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
-app.config['CORS_HEADERS'] = 'Content-Type'
-
-cors = CORS(app, resources={r"/foo": {"origins": "http://3.80.56.170:5000/"}})
 
 
 from app import views
