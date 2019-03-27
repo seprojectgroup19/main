d0a10de42d60ede303be6916517bf56909fa65b5
Cormac English <cormacenglish@gmail.com>
2019-03-27 23:12:54 +0000

    CORS

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,6 @@ from flask import Flask
 from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 from app import views
