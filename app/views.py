e17f31c0d0be200f62fa490fc8606e735c6658c7
Cormac English <cormacenglish@gmail.com>
2019-03-28 00:02:58 +0000

    s

--- app/views.py
+++ app/views.py
@@ -17,39 +17,6 @@ def index():
  return render_template("index.html", **returnDict)
 
 
-@app.route('/lookup', methods=["GET"])
-def lookup():
-    id = request.args.get('id')
-    val_list = []
-    validator = open("app/static/validation.txt", "r")
-    for line in validator:
-        val_list.append(line.rstrip())
-
-    mydb = mysql.connector.connect(
-      host=val_list[0],
-      user=val_list[1],
-      passwd=val_list[2],
-      database=val_list[3]
-    )
-
-    mycursor = mydb.cursor()
-    result_list = []
-
-    sql = "SELECT available_bike_stands, available_bikes, last_update FROM DublinBikesDB.dynamic WHERE number = '"+str(id)+"' ORDER BY last_update DESC LIMIT 1"
-    mycursor.execute(sql)
-
-    for (available_bike_stands,  available_bikes, last_update) in mycursor:
-        result_list.extend( [available_bike_stands,  available_bikes, last_update])
-    del sql
-    del mycursor
-    mycursor = mydb.cursor()
-    sql = "SELECT icon FROM DublinBikesDB.weather WHERE number = '"+str(id)+"' ORDER BY time DESC LIMIT 1;"
-    mycursor.execute(sql)
-    for (icon) in mycursor:
-        result_list.extend( [icon])
-
-    final_result = json.dumps(result_list)
-    return final_result
 
 
 
