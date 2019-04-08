b29e5140d9889d69893740a937be7044d6d2ebca
Ciaran Barron <ciaran.barron.1@ucdconnect.ie>
2019-04-08 18:37:47 +0100

    Rewrote lookup function in views to use execute_query script.

--- app/views.py
+++ app/views.py
@@ -25,43 +25,28 @@ def index():
 
 @app.route('/lookup', methods=["GET"])
 def lookup():
+
     id = request.args.get('id')
-    val_list = []
-   
-    validator = open("app/static/validation.txt", "r")
-    for line in validator:
-        val_list.append(line.rstrip())
+    
+    bikes = f"""
+        SELECT available_bike_stands, available_bikes, last_update 
+        FROM DublinBikesDB.dynamic 
+        WHERE number = {str(id)} 
+        ORDER BY last_update DESC LIMIT 1;
+    """
+    weather = f"""
+        SELECT icon 
+        FROM DublinBikesDB.weather 
+        WHERE number = {str(id)}
+        ORDER BY time DESC LIMIT 1;
+    """
+    result = list(eq.execute_sql(bikes)[0])
+    result += [tuple(eq.execute_sql(weather)[0])]
 
-    mydb = mysql.connector.connect(
-      host=val_list[0],
-      user=val_list[1],
-      passwd=val_list[2],
-      database=val_list[3]
-    )
-
-    mycursor = mydb.cursor()
-    result_list = []
-
-    sql = "SELECT available_bike_stands, available_bikes, last_update FROM DublinBikesDB.dynamic WHERE number = '"+str(id)+"' ORDER BY last_update DESC LIMIT 1"
-    mycursor.execute(sql)
-
-    for (available_bike_stands,  available_bikes, last_update) in mycursor:
-        result_list.extend( [available_bike_stands,  available_bikes, last_update])
-    del sql
-    del mycursor
-    mycursor = mydb.cursor()
-    sql = "SELECT icon FROM DublinBikesDB.weather WHERE number = '"+str(id)+"' ORDER BY time DESC LIMIT 1;"
-    mycursor.execute(sql)
-    for (icon) in mycursor:
-        result_list.extend( [icon])
-
-    final_result = json.dumps(result_list)
-    return final_result
+    return json.dumps(result)
 
 @app.route('/testpage')
 def testpage():
     
-    result = eq.execute_sql("SELECT COUNT(*) FROM DublinBikesDB.dynamic;")
-    
-    return render_template("testpage.html", **{'result':result[0][0]})
+    return render_template("testpage.html", **{'result': 5})
 
